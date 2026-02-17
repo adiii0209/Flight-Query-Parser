f076c04cfd373b4e8d252a9dc6405b1a72c03e27
adiii0209 <adityarana200502@gmail.com>
2026-02-17 21:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ from models import User, Customer, Itinerary
 from extensions import db
 
 import pytesseract
-<<<<<<< HEAD
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-=======
->>>>>>> f5a3387 (Initial commit)
 
 
 load_dotenv()
