a3d954d171a25a87b08d121cfdc53b841167a7ea
adiii0209 <adityarana200502@gmail.com>
2026-06-27 17:22:27 +0530

    remove debug/migration scripts from repo

--- find_user.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, text
-db_url = 'postgresql://neondb_owner:npg_DXS9zI4vqBkt@ep-sparkling-frost-aolg21mh-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
-engine = create_engine(db_url)
-with engine.connect() as conn:
-    res = conn.execute(text('SELECT user_id, COUNT(*) FROM ticket GROUP BY user_id')).all()
-    for r in res:
-        print(dict(r._mapping))
